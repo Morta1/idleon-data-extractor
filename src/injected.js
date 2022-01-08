cb313104ea845a3373d96f3fb22309adef7d2f39
mortaub <mortaob@gmail.com>
2022-01-08 17:45:07 +0200

    Added guild data to ext

--- src/injected.js
+++ src/injected.js
@@ -1,6 +1,6 @@
 setTimeout(async () => {
   const findData = setInterval(() => {
-    if (!l || !d || !Q) return;
+    if (!l || !d || !Q || ae) return;
     if (Object.keys(d)?.length === 0 || Object.keys(Q)?.length === 0) return;
     retry();
     window.setInterval(retry, 30000);
@@ -9,11 +9,14 @@ setTimeout(async () => {
 
   const retry = async () => {
     const rawData = await d?.collection("_data")?.doc(l)?.get();
+    const guildData = await d?.collection("_guildStat")?.doc(ae)?.get();
     let usernameList = Q.getUserNameList();
     const serializedData = rawData.data();
+    const serializedGuildData = guildData.data();
     if (serializedData && serializedData?.['AFKtarget_0'] && usernameList) {
       const send = new CustomEvent("globalData", {
         detail: {
+          serializedGuildData,
           serializedData,
           usernameList,
           lastUpdated: new Date().toString()
